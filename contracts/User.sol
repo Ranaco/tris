1a881f4616809231e5d10f94db1dbc492512d2bd
Ranaco <ranasatyamraj@gmail.com>
2022-07-14 15:43:02 +0530

    Contract updated

--- contracts/User.sol
+++ contracts/User.sol
@@ -3,18 +3,58 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "./3_TrisNFT.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract UserContract{
+contract TrisNFT is ERC721URIStorage{
+    using Counters for Counters.Counter;
+    
+    constructor() ERC721("TrisNFT", "TRI"){}   
+    Counters.Counter private _tokenId;
+    address contractAddress = address(this);
+
+    function mintToken(string memory tokenUri) public returns(uint256 itemId){
+        require(contractAddress == msg.sender, "Only the marketplace contract can call this function");
+        _tokenId.increment();
+        uint256 currentId = _tokenId.current();
+        _mint(contractAddress, currentId);
+        _setTokenURI(currentId, tokenUri);
+        return currentId;
+    }
+
+    function updateContractAddress( address marketPlaceAddress) public  {
+        require(contractAddress == address(this) || contractAddress == msg.sender, "Address is already declared or only the  owner contract can change the address.");
+        contractAddress = marketPlaceAddress;
+    }
+
+    function getLatestTokenId() public view returns(uint256 itemId){
+        return _tokenId.current();
+    }
+}
+
+contract UserContract is ReentrancyGuard{
     using Counters for Counters.Counter;
     Counters.Counter public userCount;
     mapping(address => bool) public userIsRegistered;
     mapping(address => User) private mapToUser;
+    address private trisNftAddress;
+    address private owner;
     User[] private usersList;
+    mapping(address => mapping(bytes32 => Post)) private _postsOfUsers;
+
+    constructor(address nftAddress, address ownerAddress){
+        trisNftAddress = nftAddress;
+        owner = ownerAddress;
+        TrisNFT(nftAddress).updateContractAddress(address(this));
+    }
+
+    
 
     /*
-     *   User struct for storing user data. 
-     *   name, userName, userAddress are required as in the UI the rest can be 0 or empty.
+        User struct for storing user data. 
+        name, userName, userAddress are required as in the UI the rest can be 0 or empty.
      */
     struct User{
         string name;
@@ -24,23 +64,26 @@ contract UserContract{
         string wallUrl;
         uint256 followers;
         uint256 following;
-        uint256 postCount;
-        address payable userAddress;
+        bytes32[] postCount;
+        address userAddress;
         address[] friends;
-        Post[] posts;
     }
-   
+
     /*
-     *   Post struct for storing the posts of each user.
-     *   The token id connects post to the nft in ./Tris_NFT.sol
-     *
-     *   A post can be either NFT or a regular post
+        Post struct for storing the posts of each user.
+        The token id connects post to the nft in ./Tris_NFT.sol
+
+        A post can be either NFT or a regular post
      */
     struct Post{
         string post;
-        uint256 postId;
+        bytes32 postId;
         uint256 likes;
+        bool isForSale;
+        bool postIsBought;
         uint256 commentsCount;
+        address owner;
+        address payable seller;
         string[] comments;
         uint256 priceByOwner;
         uint256 basePrice;
@@ -49,12 +92,12 @@ contract UserContract{
     }
 
     /*
-     *   Events 
+        Events 
      */
     event UserCreated (string name, string userName, string avatarUrl, address userAddress);
-    event PostUploaded(string post, uint256 postId, uint256 tokenId, uint256 priceByOwner, uint256 basePrice);
-    
-    //Function to create user
+    event PostUploaded(string post, bytes32 postId, uint256 tokenId, uint256 priceByOwner, uint256 basePrice, address owner, address seller);
+    event PostSold(string post, address owner, address seller, uint256 price);
+
     function createUser(string memory name, string memory userName, string memory avatarUrl, string memory bio, string memory wallUrl) public {
         require(userIsRegistered[msg.sender] != true, "User already exists");
         User storage userData = mapToUser[msg.sender];
@@ -65,7 +108,7 @@ contract UserContract{
         userData.wallUrl = wallUrl;
         userData.followers = 0;
         userData.following = 0;
-        userData.userAddress = payable(msg.sender);
+        userData.userAddress = msg.sender;
         userData.friends = new address[](0);
         userCount.increment();
         userIsRegistered[msg.sender] = true;
@@ -73,76 +116,117 @@ contract UserContract{
         emit UserCreated(userData.name, userData.userName, userData.avatarUrl, userData.userAddress);
     }
 
-    //Function to upload post
-    function uploadPost(string memory post, uint256 tokenId, bool isNft, uint256 priceByOwner) public {
+    function uploadPost(string memory post, bool isNft, uint256 priceByOwner, bool isForSale) public nonReentrant{
         require(userIsRegistered[msg.sender] == true, "User not registered.");
         User storage currUser = mapToUser[msg.sender];
-        currUser.postCount += 1;
+
+        uint256 tokenId;
+
+        if(isNft){
+            tokenId = TrisNFT(trisNftAddress).mintToken(post);
+        }
+
+        currUser.postCount.push(callKeccak256(post));
         Post memory currPost = Post(
             {
                 post: post,
-                postId: currUser.postCount,
+                postId: callKeccak256(post),
                 likes: 0,
+                postIsBought: false,
+                isForSale: isNft ? isForSale : false,
                 isNft: isNft,
                 commentsCount: 0,
                 priceByOwner: isNft ? priceByOwner : 0,
                 comments: new string[](0),
                 tokenId: isNft ? tokenId : 0,
-                basePrice: isNft ? 10 : 0
+                basePrice: isNft ? 10 : 0,
+                owner: isNft ? (address(this)) : (address(0)),
+                seller: isNft ? payable(msg.sender) : payable(address(0))
             }
         );
-        currUser.posts.push(currPost);
-        emit PostUploaded(currPost.post, currPost.postId, currPost.tokenId, priceByOwner, 10);
+
+        _postsOfUsers[msg.sender][callKeccak256(post)] = currPost;
+        emit PostUploaded(currPost.post, currPost.postId, currPost.tokenId, priceByOwner, 10, currPost.owner, currPost.seller);
     }
-    
-    //Function to get all the users
+
+    function buyPost(address ownerAddress, bytes32 postId, address newOwnerAddress) public payable nonReentrant{
+        Post storage currPost = _postsOfUsers[ownerAddress][postId];
+        require(userIsRegistered[msg.sender] == true, "User not registered");
+        require(msg.sender == newOwnerAddress, "Unmatched transaction, i.e., the payer should be the new owner");
+        require(userIsRegistered[ownerAddress] == true, "User not registered");
+        require(currPost.isForSale == true, "Post not for sale");
+        uint256 price = msg.value;
+        User storage newUser = mapToUser[newOwnerAddress];
+        require(price >= currPost.basePrice, "Amount should be greater than base price of the post.");
+        IERC721(trisNftAddress).transferFrom(currPost.owner, newOwnerAddress, currPost.tokenId);
+        currPost.seller.transfer(msg.value);
+        currPost.owner = (newOwnerAddress);
+        currPost.seller = payable(newOwnerAddress);
+        removePost(postId, ownerAddress);
+        _postsOfUsers[newOwnerAddress][postId] = currPost;
+        delete _postsOfUsers[ownerAddress][postId];
+        newUser.postCount.push(postId);
+    }
+
     function getUserList() public view returns(User[] memory){
         return usersList;
     }
-    
-    //Function to get the posts of the current user, i.e., msg.sender
+
+    function updatePostForSale(bytes32 id, bool isForSale) public {
+        require(userIsRegistered[msg.sender] == true, "User not registered");
+        _postsOfUsers[msg.sender][id].isForSale = isForSale;
+    }
+
     function getUserPosts() public view returns (Post[] memory userPosts){
         require(userIsRegistered[msg.sender] == true, "User not registered.");
         User storage currUser = mapToUser[msg.sender];
-        uint userPostCount = currUser.postCount;
+        uint userPostCount = currUser.postCount.length;
         Post[] memory currUserPosts = new Post[](userPostCount);
         uint currentIndex = 0;
         for(uint i = 0; i < userPostCount; i++){
-            Post storage currPost = currUser.posts[currentIndex];
-            currUserPosts[currentIndex] = currPost;
-            currentIndex += 1;
+           Post storage currPost = _postsOfUsers[msg.sender][currUser.postCount[currentIndex]];
+           currUserPosts[currentIndex] = currPost;
+           currentIndex += 1;
         }
+
         return currUserPosts;
     }
-   
-    //Function to like a post
-    function likeAPost(address postOwner, uint256 postId) public returns(Post memory post){
-        require(userIsRegistered[msg.sender] == true, "User not registered");
-        uint256 totalPosts = mapToUser[postOwner].postCount;
-        for(uint i = 0; i < totalPosts; i++){
-            if(mapToUser[postOwner].posts[i].postId == postId){
-                mapToUser[postOwner].posts[i].likes  += 1;
-                return mapToUser[postOwner].posts[i];
+
+    function removePost(bytes32 postId, address postOwner) internal {
+        bytes32[] storage postCount = mapToUser[postOwner].postCount;
+        uint index;
+        for(uint i = 0; i < postCount.length; i++){
+            if(postCount[i] == postId){
+                index = i;
+                break;
             }
         }
-    }
-
-    //Function to post a comment to the post
-    function postComment( address postOwner, string memory comment, uint256 postId ) public {
-        require(userIsRegistered[msg.sender] == true, "User not registered");
-        uint256 totalPosts = mapToUser[postOwner].postCount;
-        for(uint i = 0; i < totalPosts; i++){
-            if(mapToUser[postOwner].posts[i].postId == postId){
-                mapToUser[postOwner].posts[i].commentsCount += 1;
-                mapToUser[postOwner].posts[i].comments.push(comment);
-            }
+        for(uint i = index; i < postCount.length - 1; i++){
+            postCount[i] = postCount[i + 1];
         }
+        postCount.pop();
     }
 
-    //Function to get current user's data
+    function likeAPost(address postOwner, bytes32 postId) public {
+        require(userIsRegistered[msg.sender] == true, "User not registered");
+        Post storage currPost = _postsOfUsers[postOwner][postId];
+        currPost.likes += 1;
+    }
+
+    function postComment( address postOwner, string memory comment, bytes32 postId ) public {
+        require(userIsRegistered[msg.sender] == true, "User not registered");
+        Post storage currPost = _postsOfUsers[postOwner][postId];
+        currPost.commentsCount += 1;
+        currPost.comments.push(comment);
+    }
+
     function getUserData() public view returns(User memory){
         require(userIsRegistered[msg.sender] == true, "User not registered.");
         return mapToUser[msg.sender];
-    }
-    
+    } 
+
+    function callKeccak256(string memory post) public pure returns( bytes32 result){
+      return keccak256(abi.encodePacked(post));
+   }  
+
 }
